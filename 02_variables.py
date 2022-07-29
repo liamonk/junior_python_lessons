0d5389035c2ada8741db3ac69b62686b6d140e63
liamonk <61438758+liamonk@users.noreply.github.com>
2022-07-29 14:02:38 +1200

    Adding files

---
+++ 02_variables.py
@@ -0,0 +1,11 @@
+greeting = "hello world"
+
+fav_num = 13
+print(fav_num * 2)
+
+num_string ="5"
+print(num_string * 5)
+
+PI=3.14
+
+variable_01 ="try this"
---
+++ 03_getting_input.py
@@ -0,0 +1,11 @@
+username = input("What is your name?")
+
+
+fav_num = int(input("Favourite number?"))
+
+double = (fav_num*2)
+half = (fav_num /2)
+squared = fav_num *fav_num
+
+print("Hi {}, your favourite number is {}".format(username,fav_num))
+print("Double your favourite number is {}, half your favourite number is {}, and your favourite number squared is {}" .format(double, half, squared))
